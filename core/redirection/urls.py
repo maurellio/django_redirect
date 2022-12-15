f4c25525ac14b2c4e0f91f38b7c44421b33470c8
egorgargolin <98bariors@gmail.com>
2022-12-15 21:54:12 +0100

    +slugger auto API documentation added

--- core/redirection/urls.py
+++ core/redirection/urls.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Links API",
+      default_version='v1',
+      description="Test description",
+   ),
+   public=True,
+)
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('link/<str:slug>/', views.redirector, name='redirector'),
@@ -12,6 +25,7 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('profile/create/', views.create_link, name='create'),
     path('profile/gettoken/', views.create_token),
+    path('api/v1/swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema'),
     path('api/v1/links/', views.links_api),
     path('api/v1/links/<str:slug>', views.detail_links_api)
 ]
